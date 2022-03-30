849f0129ed7f7e28e0c73a5df52b30e1b39384ff
jayant <jayantparmar420@gmail.com>
2022-03-30 17:41:15 +0530

    Added : Program for the distrubute the cards by shuffle

--- src/com/bridgelabz/DecksOfCards.java
+++ src/com/bridgelabz/DecksOfCards.java
@@ -1,18 +1,16 @@
 package com.bridgelabz;
 
+
 import java.util.Scanner;
 
 public class DecksOfCards {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print(" Enter number of players :");
         int playerCount = sc.nextInt();
-
         CardsPlay play = new CardsPlay(playerCount);
         play.generateCards();
-        play.printCards();
         play.addPlayer();
         play.distributeCards();
         play.printCardList();
