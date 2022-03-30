d14912a1cd92aac9702cfbed1daf8900990175cf
jayant <jayantparmar420@gmail.com>
2022-03-30 16:50:09 +0530

    Added: the welcome message in Deck of cards program

---
+++ src/com/bridgelabz/DecksOfCards.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class DecksOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Decks of Cards program.");
+
+    }
+}
